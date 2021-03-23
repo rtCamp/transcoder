e017814717dd4f2f4ef832b86a95822780cc1ac5
Tremi Dkhar <tremi@dkhar.com>
2021-03-23 12:31:43 +0530

    Add escape to output attribute

--- admin/partials/rt-transcoder-admin-display.php
+++ admin/partials/rt-transcoder-admin-display.php
@@ -31,7 +31,7 @@ $current_page = transcoder_filter_input( INPUT_GET, 'page', FILTER_SANITIZE_STRI
 	<div class="transcoder-settings-boxes-wrapper">
 		<div id="transcoder-settings-boxes" class="transcoder-settings-boxes-container transcoder-setting-container">
 			<p>
-			<form method="get" action="<?php echo admin_url( 'admin.php' ); ?>">
+			<form method="get" action="<?php echo esc_attr( admin_url( 'admin.php' ) ); ?>">
 				<label for="new-api-key">
 					<?php esc_html_e( 'Enter License Key', 'transcoder' ); ?>
 				</label>
