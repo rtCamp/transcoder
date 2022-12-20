752583e63c22b13a449af12f58c58afeab39c2d6
Pavan  Patil <44057535+pavanpatil1@users.noreply.github.com>
2022-12-20 21:21:46 +0530

    Update main.sh

--- .github/ci/main.sh
+++ .github/ci/main.sh
@@ -66,7 +66,7 @@ function install_playwright(){
 # Run test for new deployed site
 function run_playwright_tests () {
     cd $GITHUB_WORKSPACE/tests/e2e-playwright
-    npm run test-e2e:playwright -- specs/01
+    npm run test-e2e:playwright -- specs/01_
 }
 
 function maybe_install_node_dep() {
